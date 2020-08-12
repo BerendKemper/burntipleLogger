fcda3de9f5d7749a648efda7dc5bb88438456473
Berend Kemper <berend_kemper@hotmail.com>
2020-08-12 16:41:45 +0200

    v0.0.0

--- README.md
+++ README.md
@@ -1,35 +1,6 @@
-# multiple-logger
+# monkey-logger
 Node JS every day a new log file and error file
 
-<pre>
-<code>// Example
-const logger = require("multiple-logger"); // WARNING not published yet
+<pre><code>npm i monkey-logger // WARNING not published yet
 
-const http = require("http");
-const server = http.createServer();
-
-const io = require('socket.io')(server);
-
-const EventEmitter = require("events");
-const socketHelper = new EventEmitter();
-
-/*
-Syntax: logger.log(...data[, callback]) 
-        logger.error(...data[, callback])
-callback is optional, takes in one parameter:   logString => { /* whatever you do with it*/ }
-//*/
-
-logger.log("testing logger", 1, 2, true, false, logString => socketHelper.emit("returnLogger", logString));
-// logs "2020-08-06T15:00:09.884+0200    testing logger      1    2     true      false"    
-
-io.on("connect", socket => {
-    // ...
-    // You could use the callback to show the logString in the front-end
-    
-    const emitReturnLogger = logString => socket.emit("returnLogger", logString);
-    socketHelper.on("returnLogger", logString => emitReturnLogger(logString));
-    socket.on("disconnect", reason => {
-        socketHelper.removeListener("returnLogger", emitReturnLogger);
-    });
-});</code>
-</pre>
+const logger = require("monkey-logger");</code></pre>
--- index.js
+++ index.js
@@ -1,80 +1,36 @@
 "use strict";
-const localTimeZoneDate = function loadLocalTimeZoneDate() {
-    const pad2 = number => number < 10 ? "0" + number : number;
-    const pad3 = number => number < 10 ? "00" + number : number < 100 ? "0" + number : number;
-    const getUTCOffset = date => {
-        const hour = date.getTimezoneOffset() / -60;
-        return { hhmm: "+" + pad2(hour) + "00", hour };
-    };
-    return Object.freeze({
-        'toISOString': (date, ms = true) => pad2(date.getFullYear()) + "-" + pad2(date.getMonth() + 1) + "-" + pad2(date.getDate()) + 
-            "T" + pad2(date.getHours()) + ":" + pad2(date.getMinutes()) + ":" + pad2(date.getSeconds()) +
-            (ms === true ? "." + pad3(date.getMilliseconds()) : "") + getUTCOffset(date).hhmm,
-        'yyyymmdd': date => pad2(date.getFullYear()) + "-" + pad2(date.getMonth() + 1) + "-" + pad2(date.getDate())
-    });
-}();
-const spaces = (number = 0, string = "") => {
-    let spaces = "";
-    for (let i = 0; i < number - string.length; i++)
-        spaces += " ";
-    return spaces;
+const path = require("path");
+const fs = require("fs");
+const _logger = {};
+const logger = {};
+class Logger {
+    constructor(type = "log", dir = "loggers", name = "monkey") {
+        return new Promise((resolve, reject) => {
+            this.dirpath = path.join(dir, type);
+            fs.mkdir(this.dirpath, { recursive: true }, err => {
+                this.filepath = path.join(this.dirpath, name + ".log");
+                this.dirpath = path.join(dir, type);
+                this.filepath = path.join(this.dirpath, name + ".log");
+                this.stream = fs.createWriteStream(this.filepath, { flags: "a+" });
+                const log = (logString) => this.stream.write(logString + "\n", "utf8");
+                log.setName = name => {
+                    this.filepath = path.join(this.dirpath, name + ".log");
+                    this.stream = fs.createWriteStream(this.filepath, { flags: "a+" });
+                };
+                if (_logger[type] instanceof Logger === false) {
+                    _logger[type] = this;
+                    logger[type] = log;
+                }
+                else
+                    reject(Error(type + " Logger already exists"));
+                resolve(log);
+            });
+        });
+    }
+    static delete(type) {
+        if (_logger[type] instanceof Logger === true)
+            delete (_logger[type]);
+        delete (logger[type]);
+    }
 };
-const pretify = (...data) => {
-    let logString = "";
-    for (let string of data) {
-        string = String(string);
-        let length = 10;
-        while (length < string.length + 4)
-            length += 5;
-        logString += string + spaces(length, string);
-    }
-    return logString;
-};
-let logStream;
-let errorStream;
-let debugStream;
-const logger = Object.freeze({
-    'log': (...data) => {
-        const callback = (typeof data[data.length - 1] === "function") ? data.pop() : () => {};
-        const logStr = localTimeZoneDate.toISOString(new Date(), true) + spaces(4) + pretify(...data);
-        console.log(logStr);
-        logStream.write(logStr + "\n", "utf8");
-        callback(logStr);
-    },
-    'logNoDate': (...data) => {
-        const callback = (typeof data[data.length - 1] === "function") ? data.pop() : () => {};
-        const logStr = pretify(...data);
-        console.log(logStr);
-        logStream.write(logStr + "\n", "utf8");
-        callback(logStr);
-    },
-    'error': (...data) => {
-        const callback = (typeof data[data.length - 1] === "function") ? data.pop() : () => {};
-        const error = localTimeZoneDate.toISOString(new Date(), true) + spaces(4) + pretify(...data);
-        console.log(error);
-        errorStream.write(error + "\n", "utf8");
-        logStream.write(error + "\n", "utf8");
-        callback(error);
-    },
-    'debug': (...data) => {
-        const callback = (typeof data[data.length - 1] === "function") ? data.pop() : () => {};
-        const debug = data.join(" ");
-        debugStream.write(debug + "\n", "utf8");
-        callback(debug);
-    }
-});
-let then = new Date();
-then.setHours(0, 0, 0, 0);
-(function nextTick() {
-    const now = new Date();
-    setTimeout(() => nextTick(), (then - now) * 0.99);
-    if (now >= then) {
-        const dateString = localTimeZoneDate.yyyymmdd(now);
-        logStream = _fs.createWriteStream("./log/" + dateString + ".log", { flags: "a+" });
-        errorStream = _fs.createWriteStream("./log/error/" + dateString + ".log", { flags: "a+" });
-        debugStream = _fs.createWriteStream("./log/debug/" + dateString + ".log", {flags: "a+" });
-        then.setHours(then.getHours() + 24, 0, 0, 0);
-    }
-    logger.log("logger tick");
-}());
-module.exports = logger;
+module.exports = Object.freeze({ Logger, logger });
