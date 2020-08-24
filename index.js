c5f7e027bfc2e9874356715f9857975b062f9593
Berend Kemper <berend_kemper@hotmail.com>
2020-08-24 10:11:30 +0200

    v1.3.0 added option autoRemoveEmpty

--- index.js
+++ index.js
@@ -5,33 +5,32 @@ const instance = new WeakMap();
 const logger = {};
 class Logger {
     /**Creates logger[type]
-     * @param {String} type
+     * @param {string} type
      * @param {Object} options 
-     * @param {String} options.dir
-     * @param {String} options.name
-     * @param {Function} options.formatter
-     * @param {Array} options.extend
-     * @returns logger[type].log*/
+     * @param {string} options.dir
+     * @param {string} options.name
+     * @param {Function} options.formatter 
+     * @param {Array} options.extend */
     constructor(type, options = {}) {
+        let { dir = "loggers", name = "monkey", autoRemoveEmpty, formatter = (data, callback) => callback(data.join(" ")), extend = [] } = options;
+        this.dirpath = path.join(dir, type);
         return new Promise((resolve, reject) => {
-            let { dir = "loggers", name = "monkey", formatter = (data, callback) => callback(data.join(" ")), extend = [] } = options;
-            this.dirpath = path.join(dir, type);
             fs.mkdir(this.dirpath, { recursive: true }, err => {
                 if (instance.get(logger[type]) instanceof Logger)
                     reject(Error(type + " Logger already exists"));
-                const log = this.log = (...data) => formatter(data, this.logString);
+                const log = this.log = logger[type] = (...data) => formatter(data, this.logString);
                 this.extended = extend.map(loggerType => {
                     const _this = instance.get(loggerType);
                     if (_this instanceof Logger) return _this;
                     else throw new TypeError("extend may only have Loggers");
                 });
+                this.autoRemoveEmpty = autoRemoveEmpty;
                 log.filepath = path.join(this.dirpath, name + ".log");
                 this.writable = fs.createWriteStream(log.filepath, { flags: "a+" });
                 this.chain = [new Promise(resolve => this.writable.once("open", () => resolve()))];
                 log.once = this.once;
                 log.setName = this.setName;
                 instance.set(log, this);
-                logger[type] = log;
                 resolve();
             });
         });
@@ -58,14 +57,18 @@ class Logger {
         }
     }
     setName = name => {
+        const oldFilepath = this.log.filepath;
         const filepath = this.log.filepath = path.join(this.dirpath, name + ".log");
         const newWritable = fs.createWriteStream(filepath, { flags: "a+" });
         const chain = this.chain;
         chain.push(new Promise(resolve => {
             const newOpened = new Promise(resolve => newWritable.once("open", () => resolve()));
-            chain[chain.length - 1].then(() => chain.shift(newOpened.then(() =>
-                resolve(this.writable = newWritable)
-            )));
+            chain[chain.length - 1].then(() => chain.shift(newOpened.then(() => {
+                resolve(this.writable = newWritable);
+                if (this.autoRemoveEmpty !== false) setTimeout(() => fs.readFile(oldFilepath, { flag: 'r' }, (err, data) => {
+                    if (err === null && data.length === 0) fs.unlinkSync(oldFilepath);
+                }), 333);
+            })));
         }));
     }
 };
